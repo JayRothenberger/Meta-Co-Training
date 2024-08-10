965749dc501c41576ee0c274223de202d9ac0914
JayRothenberger <jay.c.rothenberger@gmail.com>
2024-08-10 11:40:53 -0500

    add end to end training examples

---
+++ examples/MCT_e2e_unc.py
@@ -0,0 +1,152 @@
+# standard library imports
+import argparse
+from copy import deepcopy as copy
+import os
+# installed imports
+import torch
+import torchvision
+import wandb
+# local code imports
+from mct.dahps import DistributedAsynchronousGridSearch, sync_parameters
+
+from mct.MCT import MetaCoTrainingModel
+from mct.image_models import IMAGE_DISTANCES, IMAGE_TRANSFORMS
+from mct.models import FPFT, FinetunedLinearProbe, MLPProbe_ENS
+from mct.utils import subset_npercent_dataset
+
+
+
+def training_process(args, rank, world_size):
+    dict_args = vars(args)
+    device = int(os.environ['RANK']) % torch.cuda.device_count()
+
+
+    torch.manual_seed(13)
+    torch.cuda.set_device(device)
+
+    views = ['DINOv2', 'CLIP']
+
+    view = views[int(os.environ['RANK']) % len(views)]
+
+    trains = []
+    unlbls = []
+    vals = []
+
+    # each view gets its own weights and biases process to monitor resources and performance
+    if rank < len(views):
+        wandb.init(project=f'MCT e2e with unc {args.dataset}', entity='ai2es',
+        name=f"{rank}: {args.train_size}",
+        config={'args': dict_args})
+
+    for view in views:
+        dataset = torchvision.datasets.ImageNet(args.dataset_path, split='train', transform=IMAGE_TRANSFORMS[view])
+        val = torchvision.datasets.ImageNet(args.dataset_path, split='val', transform=IMAGE_TRANSFORMS[view])
+
+        train, unlbl = subset_npercent_dataset(dataset, percent=args.train_size * 100)
+        trains.append(train)
+        unlbls.append(unlbl)
+        vals.append(val)
+
+    num_classes = 1000
+
+    models = [IMAGE_DISTANCES[view]().model.to(device) for view in views]
+
+    for model in models:
+        model.to(device)
+    
+    shapes = []
+    with torch.no_grad():
+        for train, model, view in zip(trains, models, views):
+            print(os.environ['RANK'], view, device)
+            shapes.append(model(train[0][0].unsqueeze(0).to(device)).shape[-1])
+
+    models = [MLPProbe_ENS(model, shape, num_classes) for shape, model in zip(shapes, models)]
+
+    MCTModel = MetaCoTrainingModel(models)
+
+    # preparation stage in which the model does not alter embedder weights
+    states = MCTModel.train(args.warmup_epochs, args.warmup_epochs + 1, copy(trains), copy(unlbls), copy(vals), copy(vals), checkpoint_path=f'./chkpts/{view}_chkpt', batch_size=args.batch_size, log_interval=100, amp=True)
+
+    if args.fpft:
+        # full-parameter fine-tuning on their respective datasets (this is exclusively supervised)
+        models = [FPFT(model) for model in models]
+        
+        MCTModel = MetaCoTrainingModel(models)
+        torch.distributed.barrier()
+        states = MCTModel.train(args.fpft_epochs, args.fpft_epochs + 1, copy(trains), copy(unlbls), copy(vals), copy(vals), checkpoint_path='no_fpft_mct', batch_size=args.batch_size, log_interval=100, approx=False, amp=True)
+
+        # now the meta co-training step with the representation frozen which empirically prevents collapse
+        models = [FinetunedLinearProbe(model) for model in models]
+
+    MCTModel = MetaCoTrainingModel(models)
+    torch.distributed.barrier()
+    states = MCTModel.train(args.epochs, 0, copy(trains), copy(unlbls), copy(vals), copy(vals), checkpoint_path='no_fpft_mct_after', batch_size=args.batch_size, log_interval=100, approx=False, amp=True)
+
+    return states
+
+
+def setup(rank, world_size):
+    torch.distributed.init_process_group("nccl", rank=rank, world_size=world_size)
+
+
+def cleanup():
+    torch.distributed.destroy_process_group()
+
+
+def main(args, rank, world_size):
+    setup(rank, world_size)
+    search_space = ['train_size']
+
+    agent = sync_parameters(args, rank, search_space, DistributedAsynchronousGridSearch)
+
+    args = agent.to_namespace(agent.combination)
+
+    states = training_process(args, rank, world_size)
+
+    if rank == 0:
+        print('saving checkpoint')
+        agent.save_checkpoint(states)
+
+    print('cleanup')
+    cleanup()
+
+        
+def create_parser():
+    parser = argparse.ArgumentParser(description='MCT benchmark')
+    
+    parser.add_argument('--warmup_epochs', type=int, default=20, 
+                        help='warmup epochs (default: 10)')
+    parser.add_argument('--fpft_epochs', type=int, default=20, 
+                        help='fpft epochs (default: 10)')
+    parser.add_argument('--epochs', type=int, default=100, 
+                        help='training epochs (default: 10)')
+    parser.add_argument('-b', '--batch_size', type=int, default=512, 
+                        help='batch size for training (default: 64)')
+    parser.add_argument('-p', '--patience', type=int, default=32, 
+                        help='patience for training')
+    parser.add_argument('-tb', '--test_batch_size', type=int, default=64, 
+                        help='test batch size for training (default: 64)')
+    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3,
+                        help='learning rate for SGD (default 1e-3)')
+    parser.add_argument('--dataset', type=str, default='IN1k', metavar='e',
+                        help='embeddings over which to compute the distances')
+    parser.add_argument('--path', type=str, default='/ourdisk/hpc/ai2es/jroth/AI2ES_DL_Torch/MCT/one_percent_cold_start', help='path for hparam search directory')
+    parser.add_argument('--dataset_path', type=str, default='/ourdisk/hpc/ai2es/datasets/Imagenet/2012', help='path containing training dataset')
+    parser.add_argument('--train_size', type=float, default=[0.01],
+                        help='size of the training set (%)')
+    parser.add_argument('--balanced', type=bool, default=False, 
+                        help='Balanced dataset subsetting if true, else stratified sampling')
+    parser.add_argument('--fpft', type=bool, default=False, 
+                        help='Perform the full-parameter finetuning step')
+    
+    return parser
+
+if __name__ == '__main__':
+    parser = create_parser()
+    args = parser.parse_args()
+    
+    world_size = int(os.environ["WORLD_SIZE"])
+    rank = int(os.environ["RANK"])
+    torch.multiprocessing.set_start_method('spawn')
+
+    main(args, rank, world_size)
---
+++ examples/mct_e2e_unc.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --partition=ai2es_h100
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+# Thread count:
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:2
+# memory in MB
+#SBATCH --mem=64G
+# The %04a is translated into a 4-digit number that encodes the SLURM_ARRAY_TASK_ID
+#SBATCH --output=/ourdisk/hpc/ai2es/jroth/Meta-Co-Training/slurm/mct_out_%a.txt
+#SBATCH --error=/ourdisk/hpc/ai2es/jroth/Meta-Co-Training/slurm/mct_err_%a.txt
+#SBATCH --time=48:00:00
+#SBATCH --job-name=mct
+#SBATCH --mail-user=jay.c.rothenberger@ou.edu
+#SBATCH --mail-type=ALL
+#SBATCH --chdir=/ourdisk/hpc/ai2es/jroth/Meta-Co-Training/examples
+#SBATCH --array=[0-0]%4
+#################################################
+
+nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
+nodes_array=($nodes)
+head_node=${nodes_array[0]}
+head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+
+echo Node IP: $head_node_ip
+
+
+. /home/fagg/tf_setup.sh
+conda activate /home/jroth/.conda/envs/mct
+wandb login 6ac799cb76304b17ce74f5161bc27f7a80b6ecee
+
+srun torchrun \
+--nnodes $SLURM_JOB_NUM_NODES \
+--nproc_per_node 2 \
+--rdzv_id $RANDOM \
+--rdzv_backend c10d \
+--rdzv_endpoint "$head_node_ip:64425" \
+MCT_e2e_unc.py
